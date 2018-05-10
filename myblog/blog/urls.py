ec1ba35b4a8725dc562c0a7d2ef78f8129a93ba8
jieke <jieke918@gmail.com>
2018-05-10 22:59:34 +0800

    20180510

--- myblog/blog/urls.py
+++ myblog/blog/urls.py
@@ -3,8 +3,10 @@ from django.urls import path
 # import blog.views as bv
 from . import views
 
-
+app_name = 'blog'
 urlpatterns = [
     path('',views.index, name='index'),
-    path('test.html',views.test),
+    # path('test.html',views.test),
+    path(r'^post/(?P<pk>[0-9]+)/$', views.detail, name='detail'),
 ]
+
